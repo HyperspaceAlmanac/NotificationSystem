b9c815a435063e65a97f2320d00fb3b654264a41
Almanac <ruoyw0@gmail.com>
2021-09-16 21:07:44 -0700

    Added in structure for API calls that may be needed

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -17,5 +17,97 @@ namespace NotificationSystem.Controllers
         {
             _context = context;
         }
+
+        [HttpGet("supervisors")]
+        public IActionResult GetSupervisors()
+        {
+            try
+            {
+                // Grab list of supervisors and their ID
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPost("supervisor")]
+        public IActionResult NewSupervisor()
+        {
+            try
+            {
+                // Grab list of supervisors and their ID
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPost("user")]
+        public IActionResult NewUser()
+        {
+            try
+            {
+                // Grab list of supervisors and their ID
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPost("subscribe/{id}")]
+        public IActionResult SubsribeToSupervisor(int id)
+        {
+            try
+            {
+                // Grab list of supervisors and their ID
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPost("sendNotification")]
+        public IActionResult SendNotification(int id)
+        {
+            try
+            {
+                // Grab list of supervisors and their ID
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPost("supervisor/signin")]
+        public IActionResult SupervisorSignIn()
+        {
+            try
+            {
+                // Grab list of supervisors and their ID
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+        [HttpPost("user/signin")]
+        public IActionResult UserSignIn()
+        {
+            try
+            {
+                // Grab list of supervisors and their ID
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
     }
 }
